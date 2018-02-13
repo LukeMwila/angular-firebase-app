10505e826765afd6cd4639e22d975b2395648adc
Luke Mwila <outlier.developer@gmail.com>
2018-02-14 01:03:11 +0200

    Initial push

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    NavbarComponent,
+    LoginComponent,
+    UploadComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/image.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx'
+import { AunglarFireAuth } from 'angularfire2/auth'
+import { FirebaseApp } from 'angularfire2'
+import 'firebase/storage'
+import { GalleryImage } from '../models/galleryImage.model'
+import * as firebase from 'firebase'
+
+@Injectable()
+export class ImageService {
+
+  constructor() { }
+
+}
